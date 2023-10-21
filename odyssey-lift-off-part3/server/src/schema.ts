770e375a307e6676c725da8c96c05e5cdf62e998
samuel-yeebiyo <samoyemaneyy@gmail.com>
2023-10-21 22:39:45 +0000

    Finished third module of Apollo Graph Developer - Associate certification course

---
+++ odyssey-lift-off-part3/server/src/schema.ts
@@ -0,0 +1,45 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+    track(id: ID!): Track
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main author"
+    author: Author!
+    "The track's main illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    modules: [Module]!
+  }
+
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    title: String!
+    length: Int
+  }
+`;
